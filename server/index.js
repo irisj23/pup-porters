56d05502b04aff5ef92fbec5f92cbea26e39ec55
Yuki Yamamoto <yukiyamamoto710@gmail.com>
2021-07-10 17:11:05 -0700

    set up the development environement

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = 3000;
+
+const filePath = path.join(__dirname, '../client/dist');
+const serveStatic = express.static(filePath);
+
+app.use(serveStatic)
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+})
